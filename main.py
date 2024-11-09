129faef34b2135191a7d72cdd9c972528062fd0c
Aquil-ouadah <aquil.ouadah@edu.esiee.fr>
2024-11-09 16:14:27 +0000

    Les modifications que j'ai apporté permettent de vérifier si un nombre entier est premier ou non.

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
+"""Ce code permet de vérifier si un nombre entier est premier"""
 from math import sqrt
-
 #### Fonction secondaire
 
 
 def isprime(p):
 
-    # votre code ici
+    """Fonction qui vérifie si un nombre entier p est premier."""
+    if p < 2:
+        return False
+    if p == 2:
+        return True
+    for d in range(2, int(sqrt(p)) + 1):
+        if p % d == 0:
+            return False
+    return True
+# votre code ici
 
-    pass
 
 #### Fonction principale
 
 
 def main():
+    """Cette fonction est la fonction principale"""
+# Parcourir une plage de nombres avec un for in range
+    for t in range(1, 57):
+        if isprime(t):
+            print(isprime)
+        else:
+            print(isprime)
 
     # vos appels à la fonction secondaire ici
 
